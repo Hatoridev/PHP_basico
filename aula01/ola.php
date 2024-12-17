07d52dc0e062ae5de3e28a5e6257e1780d0c981b
Hatoridev <jonasdev21@outlook.com>
2024-12-17 16:15:38 -0300

    Adicionando arquivos da aula01 PHP basico

---
+++ aula01/ola.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!-- Define o tipo de documento como HTML5, que é a versão mais atual da linguagem HTML -->
+<html>
+<!-- Inicia a estrutura do documento HTML. A tag <html> indica o começo do conteúdo HTML da página -->
+<head>
+<!-- Inicia o cabeçalho do documento. O cabeçalho contém informações sobre a página, como o título e metadados -->
+    <meta charset="UTF-8"/>
+    <!-- Define a codificação de caracteres da página como UTF-8, que suporta quase todos os caracteres utilizados no mundo -->
+    <title>Primeiro Exemplo PHP</title>
+    <!-- Define o título da página, que aparecerá na aba do navegador -->
+</head>
+<!-- Finaliza o cabeçalho -->
+<body>
+<!-- Inicia o corpo do documento. Tudo dentro dessa tag será exibido no navegador -->
+    <h1>Testando PHP</h1>
+    <!-- Adiciona um título no corpo da página. A tag <h1> cria o maior título em HTML -->
+    <?php
+    // Inicia o código PHP (PHP: Hypertext Preprocessor)
+        echo "Ola, Mundo!";
+        // O comando echo exibe o texto "Ola, Mundo!" na página (PHP: Hypertext Preprocessor)
+    ?>
+    <!-- Finaliza o código PHP (PHP: Hypertext Preprocessor) -->
+</body>
+<!-- Finaliza o corpo do documento -->
+</html>
+<!-- Finaliza o documento HTML -->
+
